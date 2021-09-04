f01349b16c1f289f38d8fc5308383a6e0d8b069a
CloudSmall <inna.smallcloud@gmail.com>
2021-09-04 15:34:29 +0300

    Changed HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         public static void main(String[] args) {
             System.out.println( "Hello world! My name is Inna. I am work as a QA engineer. My area of expertise includes testing web and mobile application");
             System.out.print("Hello,Inna!");
+            System.out.println("Just for testing Git site");
         }
 }
